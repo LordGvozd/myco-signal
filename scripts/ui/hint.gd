3cf97e97cad07a56382134a8db799b2954ff45b8
PolinaSedikova <sfp4y2sxv8@privaterelay.appleid.com>
2026-04-19 13:57:02 +0600

    feat(gameplay): event-driven interactable zones

--- scripts/ui/hint.gd
+++ scripts/ui/hint.gd
@@ -3,11 +3,13 @@ extends Label
 
 func _ready() -> void:
 	Bus.subscribe(HintEvent, display_hint)
+	Bus.subscribe(PlayerPhraseEvent, hide_text )
+
 	
-
-
 
 func display_hint(event: HintEvent):
 	text = event.hint
 	
+func hide_text(e):
+	text = ""
 	
