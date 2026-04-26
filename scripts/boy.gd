2f78e54ba0d69c31c5355e29a1e1c6e0cc98cbca
LordGvozd <zehapo@yandex.ru>
2026-04-27 00:24:38 +0600

    feat: add mark to npc

--- scripts/boy.gd
+++ scripts/boy.gd
@@ -1,30 +1,70 @@
 extends Area2D
 
-@export var message_text: String
-@onready var message = $"../DialogBubble/Label"
+@export var messages_list: Array[String]
+@export var dark_messages_list: Array[String]
+
+@export var sprite: AnimatedSprite2D
+
+@onready var message = $"DialogBubble/Label"
+
+@onready var message_text
+var current_index: int = 0
 
 var _is_player_in: bool = false
+var _printing: bool = false
+var _show_mark: bool = true
 
 func _ready() -> void:
+	message_text = messages_list[0]
+
 	message.text = ""
-	$"../DialogBubble/Label".visible = false
-	$"../DialogBubble/TextureRect".visible = false
+	$"DialogBubble/Label".visible = false
+	$"DialogBubble/TextureRect".visible = false
 	
+	Bus.subscribe(MoodChangedEvent, func(e):
+		sprite.play("dark")
+		messages_list = dark_messages_list
+		_show_mark = false
+)
+
+func _process(delta: float) -> void:
+	if _is_player_in and Input.is_action_pressed("interact") and not _printing:
+		next_message()
+		
+func next_message():
+	if current_index >= len(messages_list) - 1:
+		_show_mark = false
+		current_index = 0
+	else:
+		current_index += 1
+	message_text = messages_list[current_index]
+	display_text()
+
+func display_text() -> void:
+	message.text = ""
+	$"DialogBubble/Label".visible = true
+	$"DialogBubble/TextureRect".visible = true
+	_is_player_in = true
+	_printing = true
+	for w in message_text:
+		await get_tree().create_timer(.1).timeout
+		if _is_player_in:
+			message.text += w
+		else:
+			break
+	_printing = false
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		$"../DialogBubble/Label".visible = true
-		$"../DialogBubble/TextureRect".visible = true
-		_is_player_in = true
-		for w in message_text:
-			await get_tree().create_timer(.2).timeout
-			if _is_player_in:
-				message.text += w
-			else:
-				break
+		$DialogBubble/TextureRect2.visible = false
+		display_text()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		current_index = 0
 		_is_player_in = false
 		message.text = ""
-		$"../DialogBubble/Label".visible = false
-		$"../DialogBubble/TextureRect".visible = false
+		$"DialogBubble/Label".visible = false
+		$"DialogBubble/TextureRect".visible = false
+		if _show_mark:
+			$DialogBubble/TextureRect2.visible = true
